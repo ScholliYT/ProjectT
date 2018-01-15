0a045af82ada5359c601c9317b00615f2f6b95e9
SMaurer <SMaurer@HGO2-20.RvWBK.local>
2018-01-15 13:55:40 +0100

    scroller geaddet

--- Engine/Scroller.java
+++ Engine/Scroller.java
@@ -1,11 +1,70 @@
-package Engine;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-import greenfoot.Actor;
+/**
+ * Superclass of all scrolling actors (all except Player)
+ * 
+ * @author Nigel Cunliffe
+ * @version 1.0
+ */
+public class Scroller extends Actor
+{
+    public static int scrollX; //The variable used when scrolling.
 
-public class Scroller extends Actor {
+    public Scroller()
+    {
+        scrollX = 0;
+    }
 
-	public Scroller() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Set the actor's location.
+     */
+    public void setLocation()
+    {
+        setLocation(getX() + scrollX, getY());
+    }
 
+    /**
+     * Move the world a certain amount when arrow keys are pressed.
+     */
+    public void checkKeyPress(int amount)
+    {
+        GameManager world = (GameManager) getWorld();
+        
+        if(Greenfoot.isKeyDown("left") &! Greenfoot.isKeyDown("right"))
+        {
+            scrollX = amount;
+
+            if(Greenfoot.isKeyDown("z"))
+            {
+                scrollX = amount + 2; // world will scroll faster if z is pressed while moving
+            }
+        }
+        else if(Greenfoot.isKeyDown("right") &! Greenfoot.isKeyDown("left"))
+        {
+            scrollX = -amount;
+
+            if(Greenfoot.isKeyDown("z"))
+            {
+                scrollX = -amount - 2; // world will scroll faster if z is pressed while moving
+            }
+        }
+        else
+        {
+            stopScroll();
+        }
+    }
+
+    /**
+     * add scrollingMethods to all actors that should scroll
+     */
+    public void scrollingMethods()
+    {
+        checkKeyPress(2);
+        setLocation();
+    }
+
+    public static void stopScroll()
+    {
+        scrollX = 0;
+    }
 }
