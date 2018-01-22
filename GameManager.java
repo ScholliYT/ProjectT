a012a7ed8090a40b5ddc9a0c1a02c14f96d60bfd
Nico Kranz <kranznico@live.de>
2018-01-22 19:36:55 +0100

    trying to stop the platform scroller at 0,0

--- GameManager.java
+++ GameManager.java
@@ -11,14 +11,16 @@ public class GameManager extends World
     public GameManager()
     {    
         super(800, 600, 1); 
+    	this.addObject(new Scroller(), 0, 0);
+    	int i = 0;
+    	do
+    	{
+    		this.addObject(new Plattform(), i, 300);
+    		i += 32;
+    	} while(i < 832);
     }
     
     public void act()
     {
-    	this.addObject(new Scroller(), 0, 0);
-    	for(int i = 0; i < 400; i = i + 32)
-    	{
-    		this.addObject(new Plattform(), i, 200);
-    	}
     }
 }
--- Plattform.java
+++ Plattform.java
@@ -1,134 +1,140 @@
 import greenfoot.*;
 
 public class Plattform extends Scroller {
-    private double x = 0;
-    private double y = 0;
-    private double velocityX; // in pixels per act() call
-    private double velocityY; // in pixels per act() call
-    private int state;
-    
+	private double x = -300;
+	private double y = 0;
+	private double velocityX; // in pixels per act() call
+	private double velocityY; // in pixels per act() call
+	private int state;
+
 
 	public Plattform()
 	{
 		setImage(new GreenfootImage("images/grass_test.png"));
 		getImage().scale(32, 32);
 	}
-	
+
 	public Plattform(double newVelocityX, double newVelocityY)
-    {
-        velocityX = newVelocityX;
-        velocityY = newVelocityY;
+	{
+		velocityX = newVelocityX;
+		velocityY = newVelocityY;
 
 		setImage(new GreenfootImage("images/grass_test.png"));
-		getImage().scale(16, 16);
-    }
-	
+		getImage().scale(32, 32);
+	}
+
 	public void act()
 	{
-		scrollingMethods();
+		if(getX() < 20 && Greenfoot.isKeyDown("left"))
+		{
+			
+		} else 
+		{
+			scrollingMethods();
+		}		
 	}
 	public void move()
 	{
 		setLocation(x + velocityX, y + velocityY);
 	}
-	
+
 	public void setLocation(double x, double y)
-    {
-        this.x = x;
-        this.y = y;
-        super.setLocation((int) x, (int) y);
-    }
-	
+	{
+		this.x = x;
+		this.y = y;
+		super.setLocation((int) x, (int) y);
+	}
+
 	public double getVelocityX()
-    {
-        return velocityX;
-    }
-	
+	{
+		return velocityX;
+	}
+
 	public double getVelocityY()
-    {
-        return velocityY;
-    }
-	
+	{
+		return velocityY;
+	}
+
 	public void delete(Class clss)
-    {
-        Actor actor = getOneIntersectingObject(clss);
-        if(actor != null) {
-            getWorld().removeObject(actor);
-        }
-    }
-	
+	{
+		Actor actor = getOneIntersectingObject(clss);
+		if(actor != null) {
+			getWorld().removeObject(actor);
+		}
+	}
+
 	public void setVelocityX(double newvelocityX)
-    {
-        velocityX = newvelocityX;
-    }
-	
+	{
+		velocityX = newvelocityX;
+	}
+
 	public void setVelocityY(double newVelocityY)
-    {
-        velocityY = newVelocityY;
-    }
-	
+	{
+		velocityY = newVelocityY;
+	}
+
 	public int getHeight()
-    {
-        return getImage().getHeight();
-    }
-	
+	{
+		return getImage().getHeight();
+	}
+
 	public int getWidth()
-    {
-        return getImage().getWidth();
-    }
-	
-    public void setState(int newState)
-    {
-        state = newState;
-    }
-	
+	{
+		return getImage().getWidth();
+	}
+
+	public void setState(int newState)
+	{
+		state = newState;
+	}
+
 	public int getState()
-    {
-        return state;
-    }
-	
+	{
+		return state;
+	}
+
 	public void removeFromWorld()
-    {
-        getWorld().removeObject(this);
-    }
-	
+	{
+		getWorld().removeObject(this);
+	}
+
 	public void setLocation(int x, int y)
-    {
-        setLocation((double) x, (double) y);
-    }
-	
+	{
+		setLocation((double) x, (double) y);
+	}
+
 	public boolean atWorldEdge()
-    {
-        if(getX() < 20 || getX() > getWorld().getWidth() - 20)
-            return true;
-        if(getY() < 20 || getY() > getWorld().getHeight() - 20)
-            return true;
-        else
-            return false;
-    }
+	{
+		if(getX() < 20 || getX() > getWorld().getWidth() - 20)
+			return true;
+		if(getY() < 20 || getY() > getWorld().getHeight() - 20)
+			return true;
+		else
+			return false;
+	}
 
-    public boolean canSee(Class clss){
-        Actor actor = getOneIntersectingObject(clss);
-        return actor != null;     
-    }
+	public boolean canSee(Class clss){
+		Actor actor = getOneIntersectingObject(clss);
+		return actor != null;     
+	}
 
-    public boolean onThis(Class clss){
-        Actor actor = getOneObjectAtOffset(0, 32, clss);
-        return actor != null;     
-    }
+	public boolean onThis(Class clss){
+		Actor actor = getOneObjectAtOffset(0, 32, clss);
+		return actor != null;     
+	}
 
-    public boolean underThis(Class clss){
-        Actor actor = getOneObjectAtOffset(0, -32, clss);
-        return actor != null;     
-    }
+	public boolean underThis(Class clss){
+		Actor actor = getOneObjectAtOffset(0, -32, clss);
+		return actor != null;     
+	}
 
-    public boolean byThis(Class clss){
-        Actor actor = getOneObjectAtOffset(32, 0, clss);
-        return actor != null;     
-    }
+	public boolean byThis(Class clss){
+		Actor actor = getOneObjectAtOffset(32, 0, clss);
+		return actor != null;     
+	}
 
-    public boolean byThis2(Class clss){
-        Actor actor = getOneObjectAtOffset(-32, 0, clss);
-        return actor != null;     
-    }
+	public boolean byThis2(Class clss){
+		Actor actor = getOneObjectAtOffset(-32, 0, clss);
+		return actor != null;     
+	}
 }
