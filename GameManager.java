893bf85507491033f10ff95ad8370e7242064e00
Simon <Simon@192.168.0.71>
2018-01-29 19:42:38 +0100

    ADD Player Sprite Platform

--- GameManager.java
+++ GameManager.java
@@ -10,18 +10,27 @@ public class GameManager extends World
 {
     public GameManager()
     {    
-        super(800, 600, 1); 
+        super(880, 720, 1); 
     	this.addObject(new Scroller(), 0, 0);
     	int i = 0;
     	do
     	{
-    		this.addObject(new Plattform(), i, 300);
+    		this.addObject(new Plattform(), i, 698);
     		i += 32;
     	} while(i < 832);
+    	int xCO = 832;
+    	int yCO =  656;
+    	do
+    	{
+    		this.addObject(new Plattform(), xCO, yCO);
+    		xCO+= 32;
+    		yCO= yCO - 32;
+    	} while(xCO < 928);
     	this.addObject(new Sprite(), 0, 0);
     	this.addObject(new Player(), 20, 0);
-    	
+    	 
     }
+    	
     public void act()
     {
     }
